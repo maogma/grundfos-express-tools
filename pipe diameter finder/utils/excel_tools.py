a2e9da52ac05a66415cfd50f2fc08037b4fb7d41
Luke Aspinwall <laspinwall@grundfos.com>
2022-09-13 17:01:18 -0500

    Made Changes

--- pipe diameter finder/utils/excel_tools.py
+++ pipe diameter finder/utils/excel_tools.py
@@ -1,5 +1,4 @@
 
-
-
 # Need to filter out old rows that have been previously removed (typically at bottom of PSDs)
-last_psd_row = psd_data[psd_data['Full Data'] == "[END]"].index.to_list()[0] # Find [END] row/index 
+class last_row:
+    last_psd_row = psd_data[psd_data['Full Data'] == "[END]"].index.to_list()[0] # Find [END] row/index 
