2332056076e8e9dfd8fef4eba20b6bb88e2050b4
Luke Aspinwall <laspinwall@grundfos.com>
2022-10-11 14:59:56 -0500

    Added version and author

--- bronze impeller removal/utils/__init__.py
+++ bronze impeller removal/utils/__init__.py
@@ -1,4 +1,10 @@
+"""This  software is proprietary to Grundfos and any use without express written conset from Grundfos is prohibited."""
+
+__version__="1.0.0"
+__authors__="Luke Aspinwall and Michael Ogma"
+
 from .mp_psd import *
 from .psd_tools import *
 from .Dataframe_tools import *
 from .file_ops import *
+
